26dcb485af5d7ded96de3ad790798dcdff712cbd
ChiefDNA <ngabiranodaniel74@gmail.com>
2025-06-23 08:50:38 +0300

    aded a homepage pointer to the package.json and made html poiners to icon.png relative

--- src/components/RouteLink.js
+++ src/components/RouteLink.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const RouteLink = ({ to , ids , text }) => {
     return (
         <div className={`rout-wrap ${ids}`}>
-            <Link to={to} > <img id={ids} src="/icon.png" alt=""/><span>{text}</span></Link>
+            <Link to={to} > <img id={ids} src="./icon.png" alt=""/><span>{text}</span></Link>
         </div>
     )
 }
